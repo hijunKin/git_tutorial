ca01259b182512df415cbf18bf5a769958de55b4
KinHijun <hijun.kin@g.pascalia.co.jp>
2023-07-29 10:12:22 +0900

    feat: 確認のため、chapter2を追加

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println("base")
 	fmt.Println("base")
+	fmt.Println("chapter2")
+	fmt.Println("chapter2")
 	fmt.Println("base")
-	fmt.Println("base")
-	fmt.Println("base")
+
+	fmt.Println("chapter2")
 }
