6696467824a4585f6001366d26da46051c2f9887
KinHijun <hijun.kin@g.pascalia.co.jp>
2023-07-29 14:29:42 +0900

    chapter7

--- main.go
+++ main.go
@@ -3,16 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("base")
-	fmt.Println("chapter6")
-	fmt.Println("chapter6")
-	fmt.Println("base")
-	fmt.Println("base")
-
-	fmt.Println("chapter6")
-	fmt.Println("chapter3-2")
-	fmt.Println("chapter3-2")
-	fmt.Println("chapter3")
-
-	fmt.Println("chapter3")
+	fmt.Println("chapter7")
 }
