b3b2ea0f61e4ab572009a21f60f736b2df523c92
KinHijun <hijun.kin@g.pascalia.co.jp>
2023-07-29 10:16:42 +0900

    feat: 7,8行目の修正

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("base")
+	fmt.Println("chapter1")
+	fmt.Println("chapter1")
 	fmt.Println("base")
 	fmt.Println("base")
-	fmt.Println("base")
-	fmt.Println("base")
+
+	fmt.Println("chapter1")
 }
