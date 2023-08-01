a8088ad611783456957438a957c6f82beda9a602
heejun <heepasc@icloud.com>
2023-08-02 08:02:02 +0900

    fix: chapter3-2

--- chapter1.go
+++ chapter1.go
@@ -4,9 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("chapter3")
+	fmt.Println("chapter3-2")
+	fmt.Println("chapter3-2")
 	fmt.Println("chapter3")
-	fmt.Println("chapter3")
-	fmt.Println("chapter3")
-	fmt.Println("chapter1")
 	fmt.Println("chapter1")
 }
